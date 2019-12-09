0739d88f27c2e4e717d4cef97157ddb711c1afb4
Dan Ungureanu <dan@ungureanu.me>
2019-12-09 21:49:02 +0200

    DEV: Add test to ensure post uploads are linked

--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 describe ::DiscourseEncrypt do
+  let(:upload) { Fabricate(:upload) }
+  let(:post) { Fabricate(:encrypt_post) }
+
   it 'registers current user fields' do
     expect(DiscoursePluginRegistry.serialized_current_user_fields)
       .to include(DiscourseEncrypt::PUBLIC_CUSTOM_FIELD, DiscourseEncrypt::PRIVATE_CUSTOM_FIELD)
@@ -11,5 +14,15 @@ describe ::DiscourseEncrypt do
   it 'registers preloaded custom fields' do
     expect(CategoryList.preloaded_topic_custom_fields).to include(DiscourseEncrypt::TITLE_CUSTOM_FIELD)
     expect(TopicList.preloaded_custom_fields).to include(DiscourseEncrypt::TITLE_CUSTOM_FIELD)
+  end
+
+  it 'links uploads in encrypted posts' do
+    Jobs.run_immediately!
+
+    post.update!(raw: "#{post.raw}\n[](#{upload.short_url})")
+    post.rebake!
+
+    expect(post.post_uploads.size).to eq(1)
+    expect(post.post_uploads.first.upload).to eq(upload)
   end
 end
