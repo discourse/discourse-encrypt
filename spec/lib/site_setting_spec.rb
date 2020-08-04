fe2b9e0ab144e70946399af424217d8dba12241f
Dan Ungureanu <dan@ungureanu.me>
2020-08-04 17:27:35 +0300

    FIX: Validate encrypted uploads extension
    Automatically adds 'encrypted' to authorized extensions and removes it
    from filenames before performing upload extension validation.

---
+++ spec/lib/site_setting_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SiteSettingExtensions do
+  let!(:default_extensions) { SiteSetting.authorized_extensions.split("|").reject { |x| x == "encrypted" } }
+
+  it "adds 'encrypted' extensions to authorized_extensions" do
+    expect(SiteSetting.authorized_extensions.split("|")).to match_array(default_extensions + ["encrypted"])
+  end
+
+  it "does not add 'encrypted' extensions if * is present" do
+    SiteSetting.authorized_extensions = "*"
+    expect(SiteSetting.authorized_extensions.split("|")).to match_array(["*"])
+  end
+
+  it "provider does not save 'encrypted' file extensions" do
+    SiteSetting.authorized_extensions += "|txt"
+    expect(SiteSetting.authorized_extensions.split("|")).to match_array(default_extensions + ["txt", "encrypted"])
+    expect(SiteSetting.provider.find(:authorized_extensions)&.value.split("|")).to match_array(default_extensions + ["txt"])
+  end
+end
---
+++ spec/lib/upload_validator_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe UploadValidatorExtensions do
+  it "removes '.encrypted' extension and validates the real extension" do
+    SiteSetting.authorized_extensions = "foo"
+
+    expect { Fabricate(:upload, original_filename: "test.foo") }.not_to raise_exception
+    expect { Fabricate(:upload, original_filename: "test.foo.encrypted") }.not_to raise_exception
+    expect { Fabricate(:upload, original_filename: "test.bar") }.to raise_exception(ActiveRecord::RecordInvalid)
+    expect { Fabricate(:upload, original_filename: "test.bar.encrypted") }.to raise_exception(ActiveRecord::RecordInvalid)
+
+    SiteSetting.authorized_extensions = "*"
+
+    expect { Fabricate(:upload, original_filename: "test.bar") }.not_to raise_exception
+    expect { Fabricate(:upload, original_filename: "test.bar.encrypted") }.not_to raise_exception
+  end
+end
