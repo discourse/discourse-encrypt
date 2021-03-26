e99fae85852033f8965006484b1ffd4033c9cd3e
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-03-26 11:08:32 +1100

    FEATURE: permanently delete a private message
    Previously we added a future to set a time for permanent deletion for encrypted messages.
    
    This feature allows user to permanently delete post even if the timer was not set. When the user clicks the delete button, we inform that message will be permanently deleted and create a timer for 1 minute. It allows the user to rollback if clicked by mistake.

---
+++ app/controllers/encrypted_post_timers_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class DiscourseEncrypt::EncryptedPostTimersController < ApplicationController
+  def create
+    delete_at = 1.minutes.from_now
+    Array.wrap(params[:post_id]).each do |post_id|
+      create_for_post(post_id, delete_at)
+    end
+    render json: { delete_at: delete_at }
+  end
+
+  def destroy
+    post = Post.with_deleted.find(params[:post_id])
+    encrypted_post_timer = EncryptedPostTimer.find_by(post: post)
+    return unless encrypted_post_timer
+    if post.is_first_post?
+      topic = Topic.with_deleted.find(post.topic_id)
+      guardian.ensure_can_recover_topic!(topic)
+    else
+      guardian.ensure_can_recover_post!(post)
+    end
+    encrypted_post_timer.destroy!
+  end
+
+  private
+
+  def create_for_post(post_id, delete_at)
+    post = Post.find(post_id)
+    return unless post.is_encrypted?
+    guardian.ensure_can_delete!(post.is_first_post? ? post.topic : post)
+
+    EncryptedPostTimer.create!(post: post, delete_at: delete_at)
+  end
+end
