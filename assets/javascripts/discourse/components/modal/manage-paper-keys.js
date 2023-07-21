455b4d11822ee17991c7de89b88cadb0996c5efd
Jarek Radosz <jradosz@gmail.com>
2023-07-21 11:43:49 +0200

    DEV: Migrate reset-key-pair to the new modal api

---
+++ assets/javascripts/discourse/components/modal/manage-paper-keys.js
@@ -0,0 +1,50 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { ajax } from "discourse/lib/ajax";
+import GeneratePaperKey from "./generate-paper-key";
+
+export default class ManagePaperKeys extends Component {
+  @service modal;
+
+  get keys() {
+    if (!this.args.model.encrypt_private) {
+      return [];
+    }
+
+    const privateKeys = JSON.parse(this.args.model.encrypt_private);
+    const keys = [];
+
+    for (const label of Object.keys(privateKeys)) {
+      if (label.startsWith("paper_")) {
+        keys.push({
+          isPaper: true,
+          label,
+          name: label.substring("paper_".length),
+        });
+      } else if (label === "passphrase") {
+        keys.unshift({
+          isPassphrase: true,
+          label: "passphrase",
+        });
+      }
+    }
+
+    return keys;
+  }
+
+  @action
+  generatePaperKey() {
+    this.modal.show(GeneratePaperKey, {
+      model: { device: false },
+    });
+  }
+
+  @action
+  delete(label) {
+    return ajax("/encrypt/keys", {
+      type: "DELETE",
+      data: { label },
+    });
+  }
+}
